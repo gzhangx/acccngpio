72f01c9a5874b557d65a0c80ace3432a0abb9d79
Gang Zhang <gzhangx@hotmail.com>
2015-06-08 03:12:28 +0000

    allow multiple ios

--- server/mwebgpio.js
+++ server/mwebgpio.js
@@ -1,12 +1,25 @@
 'use strict';
 
 var gpio = require('onoff').Gpio,
+leds = [],
 led = new gpio(14, 'out'),
 button = new gpio(4, 'in','both');
 button.watch(function(err, value){
  console.log(value);
 });
 
+var idnames = [2,3,17];
+for (var i = 0; i < idnames.length; i++) {
+  var curl = new gpio(idnames[i], 'out');
+  console.log('setting gpio '+idnames[i]+' to out');
+  curl.writeSync(1);
+  console.log('setting gpio '+idnames[i]+' to out done');
+  leds.push(curl);
+}
+for (var i = 0; i < leds.length; i++) {
+  console.log('set leds '+i);
+  leds[i].writeSync(1);
+}
 led.writeSync(1);
 
 exports.gpioControl = function(req, rsp) {
@@ -19,7 +32,7 @@ exports.gpioControl = function(req, rsp) {
   var gpioVal = parseInt(query.stat);
   if (gpioVal === 0 || gpioVal === 1) {
     console.log('gpio ' + gpioVal);
-    led.writeSync(gpioVal);
+    leds[req.id].writeSync(gpioVal);
   }
   rsp.jsonp({io: gpioVal, id: query.id});
 }
